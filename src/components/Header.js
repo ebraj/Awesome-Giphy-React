26130e7e5967e3b087932a7e5705683415f3d61e
Ebraj Gurung <gurungebraj22@gmail.com>
2021-08-13 21:52:59 +0600

    Header added

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="giphy__header">
+      <h1 className="giphy__header--title">Awesome Giphy</h1>
+      <p className="giphy__header--intro">@ebraj</p>
+    </div>
+  );
+};
+
+export default Header;
--- src/components/TrendingGiphy.js
+++ src/components/TrendingGiphy.js
@@ -3,39 +3,52 @@ import React, { useEffect, useState } from "react";
 // Importing the axios
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
+import Header from "./Header";
 
 const TrendingGiphy = () => {
+  // Creating the loader
+  const [isloading, setIsLoading] = useState(false);
   const [datas, setDatas] = useState([]);
   // Populating with the trending giphys
   useEffect(() => {
     const fetchGiphy = async () => {
+      setIsLoading(true);
       const results = await axios("https://api.giphy.com/v1/gifs/trending", {
         params: {
           api_key: "r4dAgR1xaCizzOLRhF8JHJwjbk6WrWhw",
         },
       });
       setDatas(results.data.data);
+      setIsLoading(false);
     };
     fetchGiphy();
   }, []);
 
   //   Creating the GIFS
   const renderedGiphys = () => {
+    if (isloading) {
+      return (
+        <div>
+          <p>Loading...</p>
+        </div>
+      );
+    }
     return datas.map((el) => {
       return (
         <div key={uuidv4()} className="giphy__container-el">
-          <img src={el.images.fixed_height.url} />
+          <img
+            src={el.images.fixed_height.url}
+            alt={el.images.fixed_height.url}
+          />
         </div>
       );
     });
   };
 
   return (
-    <div className="giphy container-1000">
-      <h1 className="giphy__title">Awesome Giphy</h1>
-      <div className="giphy__container">
-        {datas !== [] ? renderedGiphys() : null}
-      </div>
+    <div className="giphy container-1200">
+      <Header />
+      <div className="giphy__container">{renderedGiphys()}</div>
     </div>
   );
 };
