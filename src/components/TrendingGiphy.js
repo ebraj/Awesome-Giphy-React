5431485d69e2c579dc751a9e7ce4b0db19f0b245
Ebraj Gurung <gurungebraj22@gmail.com>
2021-08-14 08:56:15 +0600

    Search bar added

--- src/components/TrendingGiphy.js
+++ src/components/TrendingGiphy.js
@@ -2,24 +2,34 @@ import React, { useEffect, useState } from "react";
 
 // Importing the axios
 import axios from "axios";
-import { v4 as uuidv4 } from "uuid";
 import Header from "./Header";
+import { v4 as uuidv4 } from "uuid";
+import { BiSearch } from "react-icons/bi";
 
 const TrendingGiphy = () => {
   // Creating the loader
   const [isloading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [search, setSearch] = useState("");
   const [datas, setDatas] = useState([]);
   // Populating with the trending giphys
   useEffect(() => {
     const fetchGiphy = async () => {
+      setIsError(false);
       setIsLoading(true);
-      const results = await axios("https://api.giphy.com/v1/gifs/trending", {
-        params: {
-          api_key: "r4dAgR1xaCizzOLRhF8JHJwjbk6WrWhw",
-        },
-      });
-      setDatas(results.data.data);
-      setIsLoading(false);
+      // Handling the Error Properly
+      try {
+        const results = await axios("https://api.giphy.com/v1/gifs/trending", {
+          params: {
+            api_key: "r4dAgR1xaCizzOLRhF8JHJwjbk6WrWhw",
+          },
+        });
+        setDatas(results.data.data);
+        setIsLoading(false);
+      } catch (err) {
+        setIsError(true);
+        console.log(err);
+      }
     };
     fetchGiphy();
   }, []);
@@ -44,10 +54,26 @@ const TrendingGiphy = () => {
       );
     });
   };
+  // Handliing the Search
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
   return (
     <div className="giphy container-1200">
       <Header />
+      <form className="giphy__form">
+        <input
+          type="text"
+          placeholder="@Search the Giphy"
+          className="giphy__form--input"
+          onChange={handleSearch}
+          value={search}
+        />
+        <button className="giphy__form--submit">
+          <BiSearch />
+        </button>
+      </form>
       <div className="giphy__container">{renderedGiphys()}</div>
     </div>
   );
